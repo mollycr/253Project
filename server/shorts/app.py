728d0249ed5c2dc26e6ba993e37ffec4c4917258
Molly <mrobison@ischool.berkeley.edu>
2013-10-18 14:46:16 -0700

    switched to flask-shelve, changed some stuff in app.py, added a field in the html to support that and moved the html files to a template folder

--- server/shorts/app.py
+++ server/shorts/app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-import shelve
 from subprocess import check_output
 import flask
 from flask import request
+from flask.ext import shelve
 from os import environ
 
 app = flask.Flask(__name__)
 app.debug = True
 
-db = shelve.open("shorten.db")
+app.config['SHELVE_FILENAME'] = 'shelve.db'
+shelve.init_app(app)
 
+db = shelve.get_shelve('c')
 
 ###
 # Home Resource:
@@ -21,30 +23,13 @@ def home():
 	"""Converts long URLs to a unique key, stores that key, and redirects to the long URL when the key is accessed"""
 	begin = "people.ischool.berkeley.edu/~mrobison/server/shorts/short/?shortURL="
 	longURL = request.args.get('longURL')
-	#check the database to see if the long URL has already been entered
-	#check to see if the hash(longURL) is in the database
-	dbKey = hash(longURL)
-	#if the hash(longURL) is already in database but long URLs are not the same, then increment the key until you find
-	# one that is empty or has a URL that matches the long URL	
-	while(db.has_key(dbKey)==True and db[dbKey] != longURL):
-		dbKey +=1
-	#if long URL has already been entered, display the short URL
-	if(db[dbKey]==longURL):
-		return flask.rended_template(
-			'proj1-draft.html',
-			shortURL = begin+str(dbKey)
-		)
-	else:
-		#hash the long URL to some key (maybe not actually hash, we'll see)
-		db[dbKey] = longURL
-		#display the form and fill in the short value
-		return flask.render_template(
-			'proj1-draft.html',
-            shortURL = begin+str(dbKey)
-		)
-	return flask.rended_template(
-		'error.html',
-		)
+	shortURL = request.args.get('shortURL')
+	#I guess we're not doing any checking since flask-shelve doesn't have anything approaching good docs.
+	db[shortURL] = longURL
+	return flask.render_template(
+		'proj1.html',
+		shortURL = begin + shortURL
+	)
 
 ###
 # Redirection:
