285b42bd9c34eafdd0c2046d7ac267826bf0eddf
Molly <mrobison@ischool.berkeley.edu>
2013-10-30 16:36:14 -0700

    404

--- server/app.py
+++ server/app.py
@@ -15,14 +15,14 @@ shelve.init_app(app)
 db = shelve.get_shelve('c')
 
 ###
-# This is what the html page should send data at
+# This is what the html page should send data to
 ###
 @app.route('/shorts', methods=['POST'])
 def home():
 	"""Converts long URLs to a unique key, stores that key, and redirects to the long URL when the key is accessed"""
 	begin = "people.ischool.berkeley.edu/~mrobison/server/short/"
-	longURL = request.args.post('longURL')
-	shortURL = request.args.post('shortURL')
+	longURL = request.form.get('longURL')
+	shortURL = request.form.get('shortURL')
 	#I guess we're not doing any checking since flask-shelve doesn't have anything approaching good docs.
 	db[shortURL] = longURL
 	return flask.render_template(
@@ -32,11 +32,14 @@ def home():
 
 ###
 # Redirection:
-# 
+# this is 
 ###
 @app.route('/short')
 def redirect():
-	shortURL = request.args.get('shortURL')
+	shortURL = request.path
+	if("the short URL isn't there"):
+		return render_template('page_not_found.html'), 404
+
 	longURL = db[shortURL]
 	return flask.redirect(longURL)
 	#redirect to whatever long URL is associated
