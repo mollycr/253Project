937382becab5a09e4898bf6897c212325600330f
Molly <mrobison@ischool.berkeley.edu>
2013-10-31 13:18:09 -0700

    almost there

--- server/app.py
+++ server/app.py
@@ -8,10 +8,7 @@ from os import environ
 
 app = flask.Flask(__name__)
 app.debug = True
-#app.config['SERVER_NAME'] = "people.ischool.berkeley.edu/"
-#app.config['APPLICATION_ROOT'] = "~mrobison/server"
 db = shelve.open("shorts.db")
-
 
 ###
 # This is what the html page should send data to
@@ -19,8 +16,9 @@ db = shelve.open("shorts.db")
 @app.route('/shorts', methods=['POST'])
 def shorts():
 	begin = "people.ischool.berkeley.edu/~mrobison/server/short/"
-	longURL = request.form.get('longURL')
-	shortURL = request.form.get('shortURL')
+	longURL = str(request.form['long'])
+	longURL = processURL(longURL)
+	shortURL = str(request.form['short'])
 	db[shortURL] = longURL
 	return flask.render_template(
 		'proj1.html',
@@ -32,7 +30,7 @@ def shorts():
 ###
 @app.route('/short/<shortURL>')
 def short(shortURL):
-	flask.log(shortURL)
+	shortURL = str(shortURL)
 	if(db.has_key(shortURL)==False):
 		return render_template('page_not_found.html'), 404
 	longURL = db[shortURL]
@@ -41,7 +39,12 @@ def short(shortURL):
 
 @app.route('/')
 def home():
-	return flask.render_template('proj1.html', shortURL = "test")
+	return flask.render_template('proj1.html')
+
+def processURL(url):
+	#see if it's in http://www.google.com form
+	if(url[:7]=="http://"):
+		return 
 
 if __name__ == "__main__":
 	app.run(port=int(environ['FLASK_PORT']))
--- server/static/letter.js
+++ server/static/letter.js
@@ -1,31 +1,14 @@
-//function hideCurlies()
-    //{
-    //find curly brackets and hide them
-    //var curlies = /{{}}
-    //}
-
 function allLetter()
-      { 
-      var letters = /^[A-Za-z]+$/;
-      if(short.value.match(letters))
-      {
-      alert('yay! it worked!')
-      displayText();
-      }
-      
-       else
-      {
-      alert('Please input letters only.');
-      return false;
-      }
-      };
-      
-      
-      
- function displayText()
-    {
-        document.getElementById("url_text").innerHTML="Here's your new short link!";
-    };
-    
-    
-    
+{
+	var short = document.forms["thisForm"]["short"];
+	var letters = /^[A-Za-z]+$/;
+	if(short.value.match(letters))
+	{
+		return true;
+	}
+	else
+	{
+		alert('Please input letters only.');
+		return false;
+	}
+};    
