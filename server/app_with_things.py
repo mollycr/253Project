001746e6d5354f1dbd1d978c314540b6065194f5
Molly <mrobison@ischool.berkeley.edu>
2013-11-25 15:54:35 -0800

    adsf

--- server/app_with_things.py
+++ server/app_with_things.py
@@ -45,6 +45,25 @@ def login():
 			return "Incorrect password."
 		"start a session"
 
+@app.route('/', methods=['POST'])
+def createAccount():
+	#grab all existing usernames and emails from db
+	existingAccounts=dict(db.execute('''SELECT UserName,Email from User''').fetchall())
+	userName = str(request.form['username'])
+	Email=str(request.form['email'])
+	Password=str(request.form['password'])
+	if userName in existingAccounts:
+		return render_template('create_account.html', usernameError="Username is already taken")
+	if Email in existingAccounts.values():
+		return render_template('create_account.html',emailError="Email account already exists")
+	else:
+		salt = os.urandom(40)
+		h = hashlib.sha1()
+		h.update(salt)
+		h.update(Password)
+		db.execute('''INSERT INTO User VALUES(?,?,?,?,?)''',(null,userName,Email,h.hexdigest(),salt))
+		#render template Account successfully created
+
 @app.route('/')
 def home(newURL="default"):
 	if newURL=="default":
