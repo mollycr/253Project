8c9fa69e34fe0a8de4efcb8ba2310b71a4f25aed
Molly <mrobison@ischool.berkeley.edu>
2013-12-10 21:17:44 -0800

    trying for table sort

--- server/app.py
+++ server/app.py
@@ -120,18 +120,21 @@ def myAccount():
 	#generate the starting html
 	html = '''<form id="deleteLinks" action="update" method="post">
 					<input type="hidden" name="shorts" value="%(allShorts)s"/>
-					<table id="links">
+					<table id="linksTable">
+						<thead>
 						<tr>
-							<th>Long url</th>
-							<th>Short url</th>
-							<th>Number of visits</th>
+							<th data-sort="string">Long url</th>
+							<th data-sort="string">Short url</th>
+							<th data-sort="int">Number of visits</th>
 							<th>Tags</th>
-							<th>Created</th>
+							<th data-sort="string">Created</th>
 							<th>Delete?</th>
 							<th>Add tags</th>
 						</tr>
+						</thead>
+						<tbody>
 				'''
-	tableEnd = '</table> <input type="submit" value="Delete selected"/> </form>'
+	tableEnd = '</tbody></table> <input type="submit" value="Delete selected"/> </form>'
 	rowTemplate = '''
 					<tr>
 						<td> %(long)s </td>
