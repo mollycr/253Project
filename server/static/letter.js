2bd05b82457f360793a26e422da23c05004bc872
Sandra <sandra@ischool.berkeley.edu>
2013-12-12 12:55:35 -0800

    js validate home.html

--- server/static/letter.js
+++ server/static/letter.js
@@ -9,32 +9,36 @@ $("#autoCreate").click(function(){
 
 function allLetterNumber()
 {
+	$(".err").hide();
 	if ($("#long").val()==""||$("#long").val()==null){
-		alert("Please specify a URL to shorten");
-		return false;	
+		//alert("Please specify a URL to shorten");
+		var longError=true;
+		$("#noLongLink").show();
 	}
-	else if($("#autoCreate:checked").val()=="autoCreate"){
-		//autocreate is checked
-		return true;
-	}
-	else{ 
+	if($("#specify:checked").val()=="specify"){ 
 		//If button checked for user choice, ensure they have input a short URL
-		//var short = document.forms["thisForm"]["short"];
 		var short=$("#short").val();
 		var letterNumber = /^[A-Za-z0-9]+$/;
 		if (short==""||short==null){
-			alert("Please input a short URL");
-			return false;
+			$("#noShortLink").show();
+			var shortError=true;
 		}
 		else if(short.match(letterNumber))
 		{
 			return true;
 		}
 		else
+		//short link has characters other than letters and numbers
 		{
-			alert('Please input letters or numbers only.');
-			return false;
+			$("#shortLinkError").show();
+			var shortError=true;
 		}
+	}
+	if(longError||shortError){
+		return false;
+	}
+	else{
+		return true;
 	}
 }
 
